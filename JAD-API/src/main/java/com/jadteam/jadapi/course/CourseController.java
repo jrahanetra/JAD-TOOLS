c6b8389eef09fea02521cdc0610385ac4ee98b28
Antsa <antsa.rafano1@gmail.com>
2024-08-20 14:21:51 +0300

    Résolution cors error GET Courses

--- JAD-API/src/main/java/com/jadteam/jadapi/course/CourseController.java
+++ JAD-API/src/main/java/com/jadteam/jadapi/course/CourseController.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  * CourseController
  */
 @RestController
+@CrossOrigin
 @RequestMapping("/courses")
 public class CourseController {
     
