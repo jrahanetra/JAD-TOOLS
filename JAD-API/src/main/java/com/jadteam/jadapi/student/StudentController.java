f8f67b14d23a0c46c45a491e8b268ad459ab5523
Antsa <antsa.rafano1@gmail.com>
2024-08-26 16:20:50 +0300

    Correction CORS error POST student

--- JAD-API/src/main/java/com/jadteam/jadapi/student/StudentController.java
+++ JAD-API/src/main/java/com/jadteam/jadapi/student/StudentController.java
@@ -1,14 +1,15 @@
 package com.jadteam.jadapi.student;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PutMapping;
 
 
 /**
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.PutMapping;
  */
 @RestController
 @RequestMapping("/students")
+@CrossOrigin(origins = "http://localhost:3000")
 public class StudentController {
 
     private final StudentService studentService;
