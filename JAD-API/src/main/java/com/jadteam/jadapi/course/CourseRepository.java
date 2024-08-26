15062a32e434452b3b70b27c2e5fd432dcbdeb93
Antsa <antsa.rafano1@gmail.com>
2024-08-26 23:04:49 +0300

    Ajout GET StudentCourse par levelId

--- JAD-API/src/main/java/com/jadteam/jadapi/course/CourseRepository.java
+++ JAD-API/src/main/java/com/jadteam/jadapi/course/CourseRepository.java
@@ -3,6 +3,8 @@ package com.jadteam.jadapi.course;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.jadteam.jadapi.subject.Subject;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -11,5 +13,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CourseRepository extends JpaRepository<Course, Integer> {
 
     public List<Course> findAllByCourseDate(LocalDate courseDate);
+
+    public List<Course> findAllBySubject(Subject subject);
 
 }
