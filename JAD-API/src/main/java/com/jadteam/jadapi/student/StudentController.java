8eddff90a8cd5f4edef93d1c649d4f2ee51df076
Antsa <antsa.rafano1@gmail.com>
2024-08-26 16:34:46 +0300

    Ajout acceptation requêtes de prévol

--- JAD-API/src/main/java/com/jadteam/jadapi/student/StudentController.java
+++ JAD-API/src/main/java/com/jadteam/jadapi/student/StudentController.java
@@ -1,6 +1,7 @@
 package com.jadteam.jadapi.student;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -24,6 +26,11 @@ public class StudentController {
 
 	public StudentController(StudentService studentService) {
         this.studentService = studentService;
+    }
+
+    @RequestMapping(method = RequestMethod.OPTIONS)
+    public ResponseEntity<Void> options() {
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping("")
